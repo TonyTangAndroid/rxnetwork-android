f7ebf70be3b9c9c9b1fa013cd62ff27cb1760d43
TonyTang <tonytang@uber.com>
2022-06-29 17:17:04 -0700

    Make compile code up to date.

---
+++ library/src/androidTest/java/com/laimiux/rxnetwork/RxNetworkIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.laimiux.rxnetwork;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Context;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class RxNetworkIntegrationTest {
+
+  @Test
+  public void useAppContext() {
+
+  }
+}
---
+++ library/src/test/java/com/laimiux/rxnetwork/ContentObservableTest.java
@@ -0,0 +1,12 @@
+package com.laimiux.rxnetwork;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContentObservableTest {
+
+    @Test
+    public void fromBroadcast() {
+    }
+}
