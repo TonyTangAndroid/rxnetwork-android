7b079a58f14defc5fcba948518909b53157461db
Zhilu Tang <tonythompsoncmu@gmail.com>
2024-07-21 21:47:45 -0700

    Test kotlin

---
+++ library/src/test/kotlin/mix/KotlinTest.kt
@@ -0,0 +1,15 @@
+package mix
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class KotlinTest {
+  @Test
+  fun setActiveNetworkInfo_shouldBeHonored() {
+    assertThat("1").isEqualTo(1)
+  }
+}
